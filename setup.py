2af38dc4789a0cfa69340c247d23d5434626bad0
Samuel Gratzl <sam@sgratzl.com>
2020-04-29 20:11:34 +0200

    release v2.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = ["pytest"]
 setup(
     name="slack_cleaner2",
     description="Slack Cleaner2 is an improved slack cleaner version using a python first approach",
-    version="2.0.1",
+    version="2.1.0",
     license="MIT license",
     long_description=readme,
     long_description_content_type="text/markdown",
