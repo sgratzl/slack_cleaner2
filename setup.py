06ce55f18295f05f57750da5cddf0bcdbdddccd6
Samuel Gratzl <sam@sgratzl.com>
2020-09-05 13:13:27 +0200

    release 2.4.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = ["pytest"]
 setup(
     name="slack_cleaner2",
     description="Slack Cleaner2 is an improved slack cleaner version using a python first approach",
-    version="2.4.2",
+    version="2.4.3",
     license="MIT license",
     long_description=readme,
     long_description_content_type="text/markdown",
--- slack_cleaner2/_info.py
+++ slack_cleaner2/_info.py
@@ -4,4 +4,4 @@
 __author__ = """Samuel Gratzl"""
 __email__ = "sam@sgratzl.com"
 __license__ = "MIT License"
-__version__ = "2.4.2"
+__version__ = "2.4.3"
